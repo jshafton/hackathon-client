2761ef7f5ce239f7ebcc367ad8a4932c16803d4e
Jacob Shafton <jshafton@strenuus.com>
2012-11-10 10:49:33 -0600

    Initial base Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackathonClient::Application.config.secret_token = '6131ef30ea3bbe9ff9573bff8ebed3f1561f86a1dd9791c75fd0b841a9bf430e79f1050d128c9eb2ea11b07e71305123fb7a2c0aea02ca704c46bf479eb31dc2'
